ffb774579087487140b39c2ea5c687d50e577ef8
MkMushtaq <43297643+MkMushtaq@users.noreply.github.com>
2020-09-13 19:35:39 +0530

    Update de_blur.m

--- deblur/de_blur.m
+++ deblur/de_blur.m
@@ -1,49 +1,98 @@
-% This program removes blur from an image using shock filter model proposed by Osher and Rudin
-% diffusion filter u_t+(sign(u_x)sign(u_xx))u_x=0 with u(x,0)=cos(x)
+% This program removes noise from an image using linear second order
+% diffusion filter u_t=u_xx+u_yy with homogeneous neumann boundary conditions
 clc
 clear all
 
 
-u=imread('cameraman.jpg');  % Blurred signal: smooth function
-nitr=input('Enter no of iterations ');
-U=im2double(u);
+
+a=imread('cameraman.tif');
+ref=im2double(a);
+%noisy=imnoise(ref,'gaussian',0.05);
+U=ref;
+
+t=input('Enter time at which solution is needed '); % example t=0.1, 1 etc 
+dt=0.1;
+
+nitr=t/dt; % No of iterations
+r=dt;
+
+
 verbose=1;
 if verbose
     figure(verbose);
-    subplot(1,2,1);
-    imshow(u)
-    title('Blurred signal')
+    subplot(1,3,1);
+    imshow(ref);
+    title('Original image')
     drawnow;
 end
-for n=1:nitr
-    
-  
+
+for n=1:nitr   % Evolving upto 't'
+
     Umc=translateImage(U,-1,0); % Shifts down, ie, In place of U(i,j) we will have U(i-1,j)
     Upc=translateImage(U,1,0); % Shifts up, ie, In place of U(i,j) we will have   U(i+1,j)
     Ucm=translateImage(U,0,-1);% Shifts right, ie, In place of U(i,j) we will have U(i,j-1)
     Ucp=translateImage(U,0,1); % Shifts left, ie, In place of U(i,j) we will have U(i,j+1)
-    A=(U-Umc); 
-    B=(Upc-U); 
-    C=(U-Ucm); 
-    D=(Ucp-U); 
-    K=MIN(B,A);
-    L=MIN(D,C);
-    U=U-(((0.1)*sqrt((K.^2)+(L.^2))).*(sign((Umc+Upc+Ucm+Ucp-(4*U)))));
     
+    U=U+r*(Upc+Umc+Ucp+Ucm-4*U); % updating U
     
-   if verbose
+    if verbose
     figure(verbose);
-    subplot(1,2,2);
-    imshow(U)
-    title(n)
+    subplot(1,3,2);
+    imshow(U);
+    title('Blurred image')
     drawnow;
-   end
+    end
+    %pause(1) % pause the display for two seconds
 
 end
 
-function Y=MIN(a,b)
-    c=sign(a.*b);
-    c(c<=0)=0;
-    Y=sign(a).*min((sign(a).*a),(sign(b).*b));
-    Y=Y.*c;
+    %Filtered image at given 't'
+dx = 1;
+dy = 1;
+
+
+for n=1:200   % Evolving upto 't'
+
+    Umc=translateImage(U,-1,0); % Shifts down, ie, In place of U(i,j) we will have U(i-1,j)
+    Upc=translateImage(U,1,0); % Shifts up, ie, In place of U(i,j) we will have   U(i+1,j)
+    Ucm=translateImage(U,0,-1);% Shifts right, ie, In place of U(i,j) we will have U(i,j-1)
+    Ucp=translateImage(U,0,1); % Shifts left, ie, In place of U(i,j) we will have U(i,j+1)
+    deltaYP = (Upc - U)./(dx);
+    deltaYM = (U - Umc)./(dx);
+    deltaXP = (Ucp - U)./(dy);
+    deltaXM = (U - Ucm)./(dy);
+    Ux = m( deltaXP , deltaXM );
+    Uy = m( deltaYP , deltaYM );
+    c1 = ( (Ux).^2 + (Uy).^2 ).^(0.5);
+    c2 = (Upc+Umc+Ucp+Ucm-4*U)./(dx);
+    %c3 = sin(c2);
+    %c3 = c2./abs(c2);
+    c3 = tanh(c2);
+    %c3 = (abs(c2)).^(1/3);
+    c3(isnan(c3))=0;
+    U=U-dt*(c1.*c3); % updating U
+    
+    if verbose
+    figure(verbose);
+    subplot(1,3,3);
+    imshow(U);
+    title('De - Blurred image')
+    drawnow;
+    end
+    %pause(1) % pause the display for two seconds
+
+end
+
+function O = m(a,b)
+    O = a.*b;
+    for i=1:256
+        for j = 1:256
+            if O(i,j)<=0
+                O(i,j)=0;
+            else
+                s = abs(a(i,j))/a(i,j);
+                O(i,j)=s*min(abs(a(i,j)),abs(b(i,j)));
+            end
+        end
+    end
 end
